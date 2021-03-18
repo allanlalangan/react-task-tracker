8469021601d4dd497bab0ecacec0ebeef458c5c7
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2021-03-18 10:58:39 -0700

    added task components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState ([
+    {
+      id: 1,
+      text: "Doctor's Appointment",
+      day: 'Feb 5th at 2:30PM',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: "School Meeting",
+      day: 'Feb 6th at 1:30PM',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: "Grocery Shopping",
+      day: 'Feb 5th at 2:30PM',
+      reminder: false
+    }
+]
+)
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,28 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = () => {
+const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+
     return (
-        <header>
-            <h1>Task Tracker</h1>
+        <header className="header">
+            <h1>{title}</h1>
+            <Button
+            color='limegreen' 
+            text='ADD' 
+            onClick={onClick}/>
         </header>
     )
+}
+
+Header.defaultProps = {
+    title: 'Task Trackr',
+}
+
+Header.propTypes = {
+    title: PropTypes.string
 }
 
 export default Header
